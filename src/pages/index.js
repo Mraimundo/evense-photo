c5eecf0eef837b2c29f2c4b50cfc84af37db9703
Mouzinho Feliz Raimundo <mdumundo18@gmail.com>
2022-12-03 22:12:59 -0300

    feat: setup init

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Image from "next/image";
+import HomeBody from "../templates/Home/Home";
+
+export default function Home() {
+  return <HomeBody />;
+}
---
+++ src/templates/Home/Home.js
@@ -0,0 +1,6 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function HomeBody() {
+  return <h1>Home</h1>;
+}
